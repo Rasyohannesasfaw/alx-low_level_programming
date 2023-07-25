5e05aff8ba8a49c4599c793b6d665bc24358b52e
Yohannes <asfawosenyohannes@gmail.com>
2023-07-25 13:31:15 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-char *_strcpy(char *dest, char *src);void rev_string(char *s);
-
+char *_strcpy(char *dest, char *src);
+void rev_string(char *s);
+int _atoi(char *s);
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
